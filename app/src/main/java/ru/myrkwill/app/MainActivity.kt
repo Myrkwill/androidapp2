b787c368b4a00af97e54bc74f26ea02643956ef7
Mark Nagibin <myrkwill@icloud.com>
2023-01-12 15:03:44 +0300

    Add Registration activity

--- app/src/main/java/ru/myrkwill/app/MainActivity.kt
+++ app/src/main/java/ru/myrkwill/app/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.myrkwill.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton = findViewById<Button>(R.id.main_activity_register_button)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this@MainActivity, "You click me", Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ app/src/main/java/ru/myrkwill/app/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package ru.myrkwill.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
