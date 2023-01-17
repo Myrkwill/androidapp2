434c8911929a643911fbde3f9486f0d91632ac93
Mark Nagibin <myrkwill@icloud.com>
2023-01-17 13:38:58 +0300

    Add image view on EditView

--- app/src/main/java/ru/myrkwill/app/EditActivity.kt
+++ app/src/main/java/ru/myrkwill/app/EditActivity.kt
@@ -2,10 +2,28 @@ package ru.myrkwill.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import ru.myrkwill.app.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClickAddImage(view: View) = with(binding) {
+        mainImageLayout.visibility = View.VISIBLE
+        fbAddImage.visibility = View.GONE
+    }
+
+    fun onClickDeleteImage(view: View) = with(binding) {
+        mainImageLayout.visibility = View.GONE
+        fbAddImage.visibility = View.VISIBLE
     }
 }
