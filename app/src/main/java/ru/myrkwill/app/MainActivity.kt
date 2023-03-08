b874c5aafe2fdf152ecdd1f3fe3c2b975edde263
Mark Nagibin <myrkwill@icloud.com>
2023-03-09 02:47:46 +0500

    Setup fragments to activity

--- app/src/main/java/ru/myrkwill/app/MainActivity.kt
+++ app/src/main/java/ru/myrkwill/app/MainActivity.kt
@@ -2,15 +2,31 @@ package ru.myrkwill.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.myrkwill.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+        binding.bottomNavMenu.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment -> binding.bottomNavMenu.visibility = View.GONE
+                else -> binding.bottomNavMenu.visibility = View.VISIBLE
+            }
+        }
     }
 }
