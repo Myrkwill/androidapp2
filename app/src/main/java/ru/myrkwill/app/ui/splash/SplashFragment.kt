2dfd209dd7b97a3f9331ef1dee9b7ba385946d57
Mark Nagibin <myrkwill@icloud.com>
2023-03-09 02:47:10 +0500

    Add splash screen

--- app/src/main/java/ru/myrkwill/app/ui/splash/SplashFragment.kt
+++ app/src/main/java/ru/myrkwill/app/ui/splash/SplashFragment.kt
@@ -1,10 +1,15 @@
 package ru.myrkwill.app.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.myrkwill.app.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -16,6 +21,13 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+
+        lifecycleScope.launch {
+            delay(2000)
+            val action = SplashFragmentDirections.actionSplashFragmentToMainFragment()
+            findNavController().navigate(action)
+        }
+
         return binding.root
     }
 
