e148d6e8cfc4eca7608826eb6015620cc6225b1d
Mark Nagibin <myrkwill@icloud.com>
2023-03-09 00:35:32 +0500

    Remove all for create new app

--- app/src/main/java/ru/myrkwill/app/MainActivity.kt
+++ app/src/main/java/ru/myrkwill/app/MainActivity.kt
@@ -1,98 +1,16 @@
 package ru.myrkwill.app
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.widget.SearchView
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import ru.myrkwill.app.databinding.ActivityMainBinding
-import ru.myrkwill.app.db.DatabaseManager
-import ru.myrkwill.app.db.RecyclerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val databaseManager = DatabaseManager(this)
-    private val recyclerAdapter = RecyclerAdapter(ArrayList(), this)
-
-    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
-        initSearchView()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        databaseManager.open()
-        fillAdapter()
-    }
-
-    fun onClickNew(view: View) {
-        val intent = Intent(this, EditActivity::class.java)
-        startActivity(intent)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        databaseManager.close()
-    }
-
-    private fun init() = with(binding) {
-        rcView.layoutManager = LinearLayoutManager(this@MainActivity)
-        val swapHelper = swapHelper()
-        swapHelper.attachToRecyclerView(rcView)
-        rcView.adapter = recyclerAdapter
-    }
-
-    private fun initSearchView() = with(binding) {
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener,
-            android.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                fillAdapter(newText!!)
-                return true
-            }
-        })
-    }
-
-    private fun fillAdapter(text: String = "") = with(binding) {
-        job?.cancel()
-        job = CoroutineScope(Dispatchers.Main).launch {
-            val list = databaseManager.read(text)
-            recyclerAdapter.update(list)
-            tvNoElements.visibility = if (list.isNotEmpty()) View.GONE else View.VISIBLE
-        }
-    }
-
-    private fun swapHelper(): ItemTouchHelper {
-        return ItemTouchHelper(object:
-            ItemTouchHelper.SimpleCallback(0,
-                ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                recyclerAdapter.removeItem(viewHolder.bindingAdapterPosition, databaseManager)
-            }
-        })
     }
 }
--- app/src/main/java/ru/myrkwill/app/db/ListItem.kt
+++
@@ -1,8 +0,0 @@
-package ru.myrkwill.app.db
-
-class ListItem {
-    var id = 0
-    var title = "empty"
-    var desc = "empty"
-    var uri = "empty"
-}
