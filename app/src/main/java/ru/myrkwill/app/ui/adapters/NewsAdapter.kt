cecccc036a1470eda6004f3eaba0b3bcdc8fe0fe
Mark Nagibin <myrkwill@icloud.com>
2023-03-09 15:31:24 +0500

    Add news view

---
+++ app/src/main/java/ru/myrkwill/app/ui/adapters/NewsAdapter.kt
@@ -0,0 +1,56 @@
+package ru.myrkwill.app.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.myrkwill.app.databinding.ItemArticleBinding
+import ru.myrkwill.app.models.Article
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: ((Article) -> Unit)) {
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class ViewHolder(private val binding: ItemArticleBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(article: Article) = with(binding) {
+            articleTitleTextView.text = article.title
+            articleDateTextView.text = article.publishedAt
+            Glide.with(articleImageView.context)
+                .load(article.urlToImage)
+                .into(articleImageView)
+            articleImageView.clipToOutline = true
+        }
+    }
+}
