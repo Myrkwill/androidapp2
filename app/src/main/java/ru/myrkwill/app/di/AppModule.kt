a52bdef61d447cc16a21b5425e2e85e7556da428
Mark Nagibin <myrkwill@icloud.com>
2023-03-09 14:46:07 +0500

    Add Hilts annotations

---
+++ app/src/main/java/ru/myrkwill/app/di/AppModule.kt
@@ -0,0 +1,54 @@
+package ru.myrkwill.app.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.myrkwill.app.data.api.NewsService
+import ru.myrkwill.app.utils.Constants
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun baseUrl() = Constants.BASE_URL
+
+    @Provides
+    fun logging() = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    fun okHttpClient() = OkHttpClient.Builder()
+        .addInterceptor(logging())
+        .build()
+
+//    @Provides
+//    fun interceptorWithApiKeyAsQueryParameter() = Interceptor {
+//        it.request()
+//            .newBuilder()
+//            .url(it.request().url
+//                .newBuilder()
+//                .addQueryParameter("apiKey", Constants.API_KEY)
+//                .build())
+//            .build()
+//            .let { request -> it.proceed(request) }
+//    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String): NewsService =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient())
+            .build()
+            .create(NewsService::class.java)
+
+}
